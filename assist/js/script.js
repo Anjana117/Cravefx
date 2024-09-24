b66b9872dea5d1cb80bda7e20aaa4c2bfe060c1e
unknown <anjanakumari322001@gmail.com>
2024-09-24 16:58:10 +0530

    4th comment

--- assist/js/script.js
+++ assist/js/script.js
@@ -8,5 +8,40 @@ window.addEventListener('scroll',()=>{
     {
         nav1.classList.remove('scroll-down')
     }
-})
+});
 
+new Swiper('.swiper-wrapper', {
+    loop: true,
+    gapCursor: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false
+        },
+        // If we need pagination
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBultets: true
+        },
+        // Navigation arrows
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+        0: {
+        slidesPerView: 2
+        },
+        576: {
+        slidesPerView: 3
+        },
+        768: {
+        slidesPerView: 4
+        },
+        1200: {
+        slidesPerView: 6
+        }
+        }
+        });
+ 
